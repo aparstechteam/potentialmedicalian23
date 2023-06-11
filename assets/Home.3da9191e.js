08ad9a736e7fd79fc4901c1cbd518349ed130cfa
aparstechteam <79406959+aparstechteam@users.noreply.github.com>
2023-06-11 22:02:14 +0600

    potential medicalian 23 released

---
+++ assets/Home.3da9191e.js
@@ -0,0 +1,14 @@
+import{r as l,o as t,c as s,a as e,b as n,w as a,d as c}from"./index.1ab97972.js";const r={class:"text-gray-900"},_={class:"container py-10 mx-auto text-center min-h-screen grid place-items-center"},d={class:"max-w-lg rounded-2xl mx-auto p-3 space-y-5"},h=e("h1",{class:"text-2xl"},"Potential Medicalian",-1),m=e("h3",null,"23 Batch",-1),p={key:0,class:"px-3"},x=e("br",null,null,-1),u=e("h2",{class:"text-red-500 font-semibold text-lg"}," Marks Distribution ",-1),g=e("pre",null,`
+Biology-30
+Chemistry-25
+Physics-20
+English-15
+Gk-10
+
+Per question mark 1
+Negative mark 0.25
+
+Total Question: 100
+Total Marks: 100
+Exam Duration: 50 Minutes
+                    `,-1),k={class:"flex justify-center pb-3 gap-3"},y=c("Go to Exam"),v={key:1,class:"space-y-4"},b=e("p",{class:"text-center text-xl"},[c("\u0985\u0982\u09B6\u0997\u09CD\u09B0\u09B9\u09A3 \u0995\u09B0\u09BE\u09B0 \u09B8\u09AE\u09DF \u09B6\u09C7\u09B7\u0964 "),e("br")],-1),f=[b],E={__name:"Home",setup(w){const i=new Date("21 September 2024").getTime()<Date.now();return JSON.parse(localStorage.getItem("potentialmedr")),(B,N)=>{const o=l("router-link");return t(),s("div",r,[e("div",_,[e("div",d,[n(o,{to:"/",as:"div",class:"text-center uppercase"},{default:a(()=>[h,m]),_:1}),i?(t(),s("div",v,f)):(t(),s("div",p,[x,u,g,e("div",k,[n(o,{to:"/exam",class:"btn"},{default:a(()=>[y]),_:1})])]))])])])}}};export{E as default};
