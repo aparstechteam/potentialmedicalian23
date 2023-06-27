4fde936b72015eaa06d38f0ae454e5291f1c043e
aparstechteam <79406959+aparstechteam@users.noreply.github.com>
2023-06-27 16:48:40 +0600

    result published

--- assets/potMed.a90fd8cf.js
+++
@@ -1,1 +0,0 @@
-const a=[{roll:"PM228571",score:"93.75",duration:"1923911",name:"Maisha Mamun Ekush ",batch:"2022",college:"Govt Bangabandhu College Khulna "},{roll:"PM227501",score:"93",duration:"2730901",name:"Mehedi Hasan Mahim ",batch:"2022",college:"Shahid Syed Nazrul Islam College,Mymensingh "}],e="/potentialmedicalian23/assets/potMed.3da03309.png";export{e as _,a as r};
